fb34d81b8d524d96af7efcbbebd22c197e41d2cc
justin <justinlin@kitchenup.com>
2020-08-02 08:14:13 +0800

    feat: add no data

---
+++ src/components/no-data/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './style.module.scss';
+
+
+
+function NoData() {
+  return (
+    <div className={style.wrapper}>
+       <span>Let get some drinks!</span>
+    </div>
+  )
+}
+
+export default (NoData);
